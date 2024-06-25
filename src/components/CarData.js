6eff60656f9e32e361f258ee1f9d43c21798e44b
FikriAminuddin <muhammad.fikri.aminuddin.fauzi@possimo.com>
2024-06-25 18:56:23 +0800

    Initial commit

---
+++ src/components/CarData.js
@@ -0,0 +1,128 @@
+// import Golf6 from "../images/cars-big/golf6.jpg";
+// import AudiA1 from "../images/cars-big/audia1.jpg";
+// import Toyota from "../images/cars-big/toyotacamry.jpg";
+// import Bmw320 from "../images/cars-big/bmw320.jpg";
+// import Benz from "../images/cars-big/benz.jpg";
+// import Passat from "../images/cars-big/passatcc.jpg";
+
+// new car
+import axia from "../images/cars-big/axia.png";
+import bezza from "../images/cars-big/bezza.png";
+import alza from "../images/cars-big/alza.png";
+import saga from "../images/cars-big/saga.png";
+
+export const CAR_DATA = [
+  [
+    {
+      name: "Bezza",
+      price: "30",
+      img: bezza,
+      model: "Bezza 1.0 G",
+      mark: "Perodua",
+      year: "2023",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Axia",
+      price: "40",
+      img: axia,
+      model: "Axia 1.0L AV",
+      mark: "Perodua",
+      year: "2023",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Alza",
+      price: "70",
+      img: alza,
+      model: "Alza - Family 7 Seater MPV",
+      mark: "Perodua",
+      year: "2023",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Petrol",
+    },
+  ],
+  [
+    {
+      name: "Saga",
+      price: "30",
+      img: saga,
+      model: "Saga 1.3L STANDARD MT",
+      mark: "Proton",
+      year: "2023",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Petrol",
+    },
+  ],
+  // [
+  //   {
+  //     name: "Toyota Camry",
+  //     price: "30",
+  //     img: Toyota,
+  //     model: "Camry",
+  //     mark: "Toyota",
+  //     year: "2006",
+  //     doors: "4/5",
+  //     air: "Yes",
+  //     transmission: "Automatic",
+  //     fuel: "Hybrid",
+  //   },
+  // ],
+  // [
+  //   {
+  //     name: "BMW 320 ModernLine",
+  //     price: "35",
+  //     img: Bmw320,
+  //     model: "320",
+  //     mark: "BMW",
+  //     year: "2012",
+  //     doors: "4/5",
+  //     air: "Yes",
+  //     transmission: "Manual",
+  //     fuel: "Diesel",
+  //   },
+  // ],
+  // [
+  //   {
+  //     name: "Mercedes-Benz GLK",
+  //     price: "50",
+  //     img: Benz,
+  //     model: "Benz GLK",
+  //     mark: "Mercedes",
+  //     year: "2006",
+  //     doors: "4/5",
+  //     air: "Yes",
+  //     transmission: "Manual",
+  //     fuel: "Diesel",
+  //   },
+  // ],
+  // [
+  //   {
+  //     name: "VW Passat CC",
+  //     price: "25",
+  //     img: Passat,
+  //     model: "Passat CC",
+  //     mark: "Volkswagen",
+  //     year: "2008",
+  //     doors: "4/5",
+  //     air: "Yes",
+  //     transmission: "Automatic",
+  //     fuel: "Gasoline",
+  //   },
+  // ],
+  
+];
---
+++ src/components/Footer.jsx
@@ -0,0 +1,90 @@
+import { IconMail, IconPhoneCall } from "@tabler/icons-react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="footer-content">
+            <ul className="footer-content__1">
+              <li>
+                <span>CAR</span> Rental
+              </li>
+              <li>
+                We offers a big range of vehicles for all your driving needs. We
+                have the perfect car to meet your needs.
+              </li>
+              <li>
+                <a href="tel:+60123456789">
+                  <IconPhoneCall /> &nbsp; (+6012) -345-6789
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="mailto: 
+                carrental@gmail.com"
+                >
+                  <IconMail />
+                  &nbsp; carrental@gmail.com
+                </a>
+              </li>
+
+              <li>
+                <a
+                  style={{ fontSize: "14px" }}
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://fikriaminuddin.github.io/"
+                >
+                  Design by FikriAminuddin
+                </a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Company</li>
+              <li>
+                <a href="#home">Pasir Mas</a>
+              </li>
+              <li>
+                <a href="#home">Careers</a>
+              </li>
+              <li>
+                <a href="#home">Mobile</a>
+              </li>
+              <li>
+                <a href="#home">Blog</a>
+              </li>
+              <li>
+                <a href="#home">How we work</a>
+              </li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Working Hours</li>
+              <li>Mon - Fri: 9:00AM - 6:00PM</li>
+              <li>Sat: Closed</li>
+              <li>Sun: Closed</li>
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Subscription</li>
+              <li>
+                <p>Subscribe your Email address for latest news & updates.</p>
+              </li>
+              <li>
+                <input type="email" placeholder="Enter Email Address"></input>
+              </li>
+              <li>
+                <button className="submit-email">Submit</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
